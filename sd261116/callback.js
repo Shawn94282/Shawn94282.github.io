2efe45075cf4ef9dd90ccd5e26ba6f7517fafd3c
Shawn94282 <dmitry_vansovich@mail.ru>
2016-12-03 12:53:34 +0400

    Add files via upload

---
+++ sd261116/callback.js
@@ -0,0 +1,25 @@
+var secret_key = 'cARiagBApt7aUjPpiSl2';
+
+if(item == item1) {
+	var response['response'] = array( 
+          'item_id' => 25, 
+          'title' => '300 золотых монет', 
+          'photo_url' => 'http://somesite/images/coin.jpg', 
+          'price' => 5 
+    ); 
+    return; 	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
